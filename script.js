def8581594adf3a9ace493109e0c435805c3736d
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-03-14 01:29:25 +0100

    api cities

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        const customizedIcon = customizeWeatherIcon(icon);
         document.querySelector(".city").innerHTML = "Weather in " + name;
         document.querySelector(".icon").src = customizeWeatherIcon(icon);
         document.querySelector(".description").innerHTML = description;
@@ -71,222 +70,64 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
         search.value = "";
     }
 })
-function getIcon(condition) {
-    if (condition === "partly-cloudy-day") {
-        return "https://i.ibb.co/PZQXH8V/27.png";
-    } else if (condition === "partly-cloudy-night") {
-        return "https://i.ibb.co/Kzkk59k/15.png";
-    } else if (condition === "rain") {
-        return "https://i.ibb.co/kBd2NTS/39.png";
-    } else if (condition === "clear-day") {
-        return "https://i.ibb.co/rb4rrJL/26.png";
-    } else if (condition === "clear-night") {
-        return "https://i.ibb.co/1nxNGHL/10.png";
-    } else {
-        return "https://i.ibb.co/rb4rrJL/26.png";
-    }
-}
 
 
-const cities = [
-    { name: "Malaga" },
-    { name: "Marrakech" },
-    { name: "Rabat" },
-    { name: "New York" },
-    { name: "Casablanca" },
-    { name: "Madrid" },
-    { name: "Tokyo" },
-    { name: "Multan" },
-    { name: "Paris" },
-    { name: "London" },
-    { name: "Istanbul" },
-    { name: "Rome" },
-    { name: "Amsterdam" },
-    { name: "Barcelona" },
-    { name: "milan" },
-    { name: "seoul" },
-    { name: "osaka" },
-    { name: "pattaya" },
-    { name: "bali" },
-    { name: "bangkok" },
-    { name: "dubai" },
-    { name: "makkah" },
-    { name: "FLORENCE" },
-    { name: "Rhodes" },
-    { name: "Porto" },
-    { name: "Nice" },
-    { name: "Stockholm" },
-    { name: "Frankfurt" },
-    { name: "Warsaw" },
-    { name: "Kraków" },
-    { name: "Heraklion" },
-    { name: "Copenhagen" },
-    { name: "Lisbon" },
-    { name: "Budapest" },
-    { name: "Munich" },
-    { name: "Brussels" },
-    { name: "Saint Petersburg" },
-    { name: "Florence" },
-    { name: "Dublin" },
-    { name: "Venice" },
-    { name: "Moscow" },
-    { name: "Berlin" },
-    { name: "Athens" },
-    { name: "Vienna" },
-    { name: "Amsterdam" },
-    { name: "Prague" },
-    { name: "Antalya" },
-    { name: "Antalya" },
-    { name: "Miami" },
-    { name: "Los Angeles" },
-    { name: "Orlando" },
-    { name: "San Francisco" },
-    { name: "Las Vegas" },
-    { name: "Honolulu " },
-    { name: "Washington" },
-    { name: "Boston" },
-    { name: "Chicago" },
-    { name: "Vancouver" },
-    { name: "Toronto" },
-    { name: "Montreal" },
-    { name: "Hong Kong" },
-    { name: "Ottawa" },
-    { name: "Quebec" },
-    { name: "Shanghai" },
-    { name: "São Paulo" },
-    { name: "Mexico City" },
-    { name: "Cairo" },
-    { name: "Mumbai" },
-    { name: "Beijing" },
-    { name: "Dhaka" },
-    { name: "Osaka" },
-    { name: "Karachi" },
-    { name: "Buenos Aires" },
-    { name: "Chongqing" },
-    { name: "Istanbul" },
-    { name: "Kolkata" },
-    { name: "Manila" },
-    { name: "Lagos" },
-    { name: "Casablanca" },
-    { name: "Rabat" },
-    { name: "Fez" },
-    { name: "Marrakech" },
-    { name: "Tangier" },
-    { name: "Agadir" },
-    { name: "Meknes" },
-    { name: "Oujda" },
-    { name: "Kenitra" },
-    { name: "Tetouan" },
-    { name: "Safi" },
-    { name: "Essaouira" },
-    { name: "El Jadida" },
-    { name: "Nador" },
-    { name: "Settat" },
-    { name: "Khouribga" },
-    { name: "Beni Mellal" },
-    { name: "Taza" },
-    { name: "Khémisset" },
-    { name: "Taourirt" },
-    { name: "Berkane" },
-    { name: "Ksar El Kebir" },
-    { name: "Rio de Janeiro" },
-    { name: "Tianjin" },
-    { name: "Kinshasa" },
-    { name: "Guangzhou" },
-    { name: "Los Angeles" },
-    { name: "Shenzhen" },
-    { name: "Lahore" },
-    { name: "Bangalore" },
-    { name: "Bogotá" },
-    { name: "Jakarta" },
-    { name: "Chennai" },
-    { name: "Lima" },
-    { name: "Bangkok" },
-    { name: "Nagoya" },
-    { name: "Hyderabad" },
-    { name: "Tehran" },
-    { name: "Chicago" },
-    { name: "Chengdu" },
-    { name: "Nanjing" },
-    { name: "Wuhan" },
-    { name: "Ho Chi Minh City" },
-    { name: "Luanda" },
-    { name: "Ahmedabad" },
-    { name: "Kuala Lumpur" },
-    { name: "Xi'an" },
-    { name: "Dongguan" },
-    { name: "Hangzhou" },
-    { name: "Foshan" },
-    { name: "Shenyang" },
-    { name: "Riyadh" },
-    { name: "Baghdad" },
-    { name: "Santiago" },
-    { name: "Surat" },
-    { name: "Suzhou" },
-    { name: "Pune" },
-    { name: "Harbin" },
-    { name: "Toronto" },
-    { name: "Dar es Salaam" },
-    { name: "Belo Horizonte" },
-    { name: "Singapore" },
-    { name: "Fukuoka" },
-    { name: "Khartoum" },
-    { name: "Barcelona" },
-    { name: "Johannesburg" },
-    { name: "Saint Petersburg" },
-    { name: "Qingdao" },
-    { name: "Dalian" },
-    { name: "Yangon" },
-    { name: "Alexandria" },
-    { name: "Jinan" },
-    { name: "Guadalajara" },
-];
 
 const search = document.querySelector("#city-input");
 
 
+
 var currentFocus;
-search.addEventListener("input", function (e) {
-    removeSuggestions();
-    var a,
-        z,
-        b,
-        i,
-        val = this.value;
-    if (!val) {
-        return false;
+
+// search.addEventListener("input", function (e, city) {
+//     removeSuggestions();
+//     var a, b, i, z, val = this.value;
+//     if (!val) {
+//         return false;
+//     }
+//     currentFocus = -1;
+//     a = document.createElement("ul");
+//     a.setAttribute("id", "suggestionsList");
+//     z = document.querySelector("#suggestions")
+//     z.appendChild(a)
+//     this.parentNode.appendChild(a);
+//     const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + $city + "&units=metric&appid=" + apiKey;
+//     fetch(apiUrl + val)
+//         .then(response => response.json())
+//         .then(data => {
+//             data.forEach(city => {
+//                 b = document.createElement("li");
+//                 b.innerHTML =
+//                     "<strong>" + city.name.substr(0, val.length) + "</strong>";
+//                 b.innerHTML += city.name.substr(val.length);
+//                 b.innerHTML += "<input type='hidden' value='" + city.name + "'>";
+//                 b.addEventListener("click", function (e) {
+//                     search.value = this.getElementsByTagName("input")[0].value;
+//                     removeSuggestions();
+//                 });
+
+//                 a.appendChild(b);
+//             });
+//         });
+// });
+async function suggestCities(query, api_key) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${api_key}`;
+    const response = await fetch(url);
+    if (response.ok) {
+        const results = await response.json();
+        const cities = results.map(result => result.name);
+        return cities;
+    } else {
+        return [];
     }
-    currentFocus = -1;
-    z = document.querySelector("#suggestions")
-    a = document.createElement("ul");
-    a.setAttribute("id", "suggestionsList");
+}
 
-    this.parentNode.appendChild(a);
-    z.appendChild(a)
-
-    for (i = 0; i < cities.length; i++) {
-        if (
-            cities[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()
-        ) {
-            b = document.createElement("li");
-            b.innerHTML =
-                "<strong>" + cities[i].name.substr(0, val.length) + "</strong>";
-            b.innerHTML += cities[i].name.substr(val.length);
-            b.innerHTML += "<input type='hidden' value='" + cities[i].name + "'>";
-            b.addEventListener("click", function (e) {
-                search.value = this.getElementsByTagName("input")[0].value;
-                removeSuggestions();
-            });
-
-            a.appendChild(b);
-        }
-    }
-});
 
 function removeSuggestions() {
     var x = document.getElementById("suggestionsList");
     if (x) x.parentNode.removeChild(x);
 }
+
 
 search.addEventListener("keydown", function (e) {
     var x = document.getElementById("suggestionsList");
